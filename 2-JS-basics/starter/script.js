caf7258895dbcd851c1fa2abd7a3dda487e934f7
Orsi <pal.orsolya@gmail.com>
2020-07-25 17:49:14 +0200

    Add files via upload

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,15 @@
+console.log("Hello world js");
+
+var firstName = 'John';
+var age = 27;
+var isMarried = false;
+
+var lastName = prompt('What is his last name?');
+
+console.log(firstName + ' ' + lastName + " is "
++ age + ' years old. Is he married? ' + isMarried);
+
+age = 'twenty eight';
+
+console.log(firstName + ' ' + lastName + " is "
++ age + ' years old. Is he married? ' + isMarried);
