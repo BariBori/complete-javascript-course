abdb96410fd16518e896c3ca674564079c8aeaec
pOrsolya <pal.orsolya@gmail.com>
2020-07-26 15:08:16 +0200

    solve challenge 1

---
+++ 1-Challenges/1challenge.js
@@ -0,0 +1,14 @@
+var JohnMass = 97;
+var JohnHeight = 1.86;
+var MarkMass = 79;
+var MarkHeight = 1.78;
+var JohnBMI = (JohnMass /(JohnHeight * JohnHeight)).toFixed(2);
+var MarkBMI = (MarkMass /(MarkHeight * MarkHeight)).toFixed(2);
+
+var MarkBMIHigher = MarkBMI > JohnBMI;
+
+console.log("-----First challenge---------");
+console.log("Mark's BMI: " + MarkBMI);
+console.log("John's BMI: " + JohnBMI);
+console.log("Is Mark BMI higher than John's? " + MarkBMIHigher);
+console.log("------------------");
