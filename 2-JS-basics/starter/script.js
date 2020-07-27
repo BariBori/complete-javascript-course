b4a412d603dba37150e6c595edbc2bc85a7fdc98
pOrsolya <pal.orsolya@gmail.com>
2020-07-27 13:12:01 +0200

    add funcion to script js update challenge index html

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -25,3 +25,21 @@ console.log('John\'s age: '+ age);
 
 var drink = age >=18 ? 'beer' : 'juice';
 console.log('John drinks ' + drink);
+
+function calculateAge(birthYear){
+    return 2020 - birthYear;
+}
+
+function yearsUntilRetirement(year, name){
+    var age = calculateAge(year);
+    var retirement = 65 - age;
+    if(retirement < 0){
+        console.log(name + ' is already retired ' + Math.abs(retirement) + ' years ago');
+    } else {
+        console.log(name + ' is ' + age +' years old. '+ name + ' retires in ' + retirement + ' years.' );
+    }
+    
+}
+
+yearsUntilRetirement(1979, 'Orsi');
+yearsUntilRetirement(1948, 'John');
