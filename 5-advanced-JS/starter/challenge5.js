34e815e4783fbc88f0a115ab9636aa942c02bd99
pOrsolya <pal.orsolya@gmail.com>
2020-09-14 18:50:33 +0200

    add alternate solution to challenge 5

--- 5-advanced-JS/starter/challenge5.js
+++ 5-advanced-JS/starter/challenge5.js
@@ -1,8 +1,8 @@
+(function(){
 var Question = function(question, answears, rightAnswear){
     this.question = question;
     this.answears = answears;
-    this.rightAnswear = rightAnswear;
-    
+    this.rightAnswear = rightAnswear;   
 }
 
 var questionHungary = new Question('What is Hungary\'s capital?', ['London', 'Ankara', 'Athens', 'Budapest'], 3);
@@ -10,33 +10,51 @@ var questionGreatBritann = new Question('What is Great Britann\'s capital?', ['L
 var questionGreece = new Question('What is Greece\'s capital?', ['London', 'Ankara', 'Athens', 'Budapest'], 2);
 var questionTurkey = new Question('What is Turkey\'s capital?', ['London', 'Ankara', 'Athens', 'Budapest'], 1);
 
-var questionsArr = [];
-questionsArr.push(questionHungary);
+var questionsArr = [questionGreatBritann, questionGreece, questionTurkey, questionHungary];
+/*questionsArr.push(questionHungary);
 questionsArr.push(questionGreatBritann);
 questionsArr.push(questionGreece);
-questionsArr.push(questionTurkey);
+questionsArr.push(questionTurkey);*/
 var score = 0;
 
 function randomQuestion(arr){
+    var randomNumber = Math.floor(Math.random()*arr.length);
+    writeQuestion(arr, randomNumber);
+
+    var answear = prompt(arr[randomNumber].question + "\nType exit to quit");
+    var correct = arr[randomNumber].rightAnswear;
+
+    checkAnswear(answear, correct);
     
-    var randomNumber = Math.floor(Math.random()*4);
+}
+
+function writeQuestion(arr, randomNumber){ 
     console.log(arr[randomNumber].question);
     for(var i=0; i<arr.length; i++){
         console.log(i + ": " + arr[randomNumber].answears[i]);
     }
-    var answear = prompt(arr[randomNumber].question);
-    if(answear == arr[randomNumber].rightAnswear){
-        console.log('Correct');
-        score = score + 1;
-        console.log('Your score: ' + score);
+}
+
+function displayScore(score){
+    console.log('YOUR SCORE: ' + score);
+    console.log('----------------------');
+}
+
+function checkAnswear(answear, correct){
+    if(answear == correct){
         console.log('----------------------');
+        console.log('CORRECT ANSWEAR');
+        score = score + 1;
+        displayScore(score);
         newquestion();
     } else if(answear === 'exit'){
-        console.log('Game end');
-    } else {
-        console.log('Wrong');
-        console.log('Your score: ' + score);
         console.log('----------------------');
+        console.log('GAME END');
+        displayScore(score);
+    } else {
+        console.log('----------------------');
+        console.log('WRONG ANSWEAR');
+        displayScore(score);
         newquestion();
     }
 }
@@ -47,4 +65,4 @@ function newquestion(){
 
 newquestion();
 
-
+})();
---
+++ 5-advanced-JS/starter/challenge5Alter.js
@@ -0,0 +1,77 @@
+
+function Question(question, answears, correct){
+    this.question = question;
+    this.answears = answears;
+    this.correct = correct;
+}
+
+Question.prototype.displayQuestion = 
+function(){
+    console.log(this.question);
+
+    for(i=0; i<this.answears.length; i++){
+        console.log(i+ ": "+this.answears[i]);
+    }
+}
+
+Question.prototype.checkAnswear = 
+function(ans, scoreCounterFunction){
+    var sc;
+    if(ans === this.correct){
+        console.log('CORRECT ANSWEAR');
+        sc = scoreCounterFunction(true);
+    } else {
+        console.log('WRONG ANSWEAR');
+        sc = scoreCounterFunction(false);
+    }
+
+    this.displayScore(sc);
+}
+
+Question.prototype.displayScore=
+function(score){
+    console.log('Your score is: ' + score);
+    console.log('----------------------');
+}
+
+var q1 = new Question('What is Hungary\'s capital?', ['Ankara', 'Athens', 'London', 'Budapest', 'Rome', 'Lisbon'], 3);
+var q2 = new Question('What is Great Britann\'s capital?', ['London', 'Ankara', 'Athens'], 0);
+var q3 = new Question('What is Greece\'s capital?', [ 'Athens', 'London', 'Ankara', 'Stockholm', 'Budapest'], 2);
+var q4 = new Question('What is Turkey\'s capital?', ['London', 'Ankara', 'Bucarest', 'Berlin','Athens', 'Budapest'], 1);
+
+var questions = [q1, q2, q3, q4];
+
+
+function countScore(){
+    var score = 0;
+    return function(correct){
+        if(correct){
+            score++;
+        }
+        return score;
+    }
+}
+
+var keepScore = countScore();
+
+function nextQuestion(){
+    var randomNumber = Math.floor(Math.random()*questions.length);
+
+    questions[randomNumber].displayQuestion();
+    
+    var answear = prompt(questions[randomNumber].question);
+
+    if(answear !== 'exit'){
+        questions[randomNumber].checkAnswear(parseInt(answear), keepScore);
+
+        nextQuestion();
+    } else {
+        console.log('GAME END');
+        console.log('Your final score is: ' + keepScore());
+    }
+}
+
+nextQuestion();
+
+
+
