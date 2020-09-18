fcf31a1b963ab506117c74643eceba45b68dc372
pOrsolya <pal.orsolya@gmail.com>
2020-09-18 15:19:28 +0200

    add functions to the budgetController function

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -35,13 +35,75 @@ var controller = (function(budgetCrtl, UICtrl){
 
 //BUDGET CONTROLLER
 var budgetController = (function(){
-   
+    //private functions
+   var Expense = function(id, description, value){
+       this.id = id;
+       this.description = description;
+       this.value = value;
+   };
+
+    var Income = function(id, description, value){
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals:{
+            exp:0,
+            inc:0
+        }
+    };
+
+    //public functions
+    return{
+        addItem: function(type, desc, val){
+            var newItem, ID;
+
+            //create new ID
+            //example: expenses array's last element's ID number + 1
+            //expArray[expArray.length-1].id+1
+
+            if(data.allItems[type].length >0){
+                ID= data.allItems[type][data.allItems[type].length-1].id + 1;
+            } else {
+                ID = 0;
+            }
+
+            //create new item based on inc and exp types
+
+            if(type === 'exp'){
+                newItem = new Expense(ID, desc, val);
+            } else if(type === 'inc'){
+                newItem = new Income(ID, desc, val);
+            }
+
+            //push it into the data structure
+
+            data.allItems[type].push(newItem);
+            
+
+            //return the new element
+            return newItem;
+
+        },
+
+        testing: function(){
+            console.log(data);
+        }
+
+    }
 
 })();
 
 //UI CONTROLLER
 var UIcontroller = (function(){
 
+    //private object
     var DOMStrings = {
         inputType: '.add__type',
         inputDescription: '.add__description',
@@ -49,6 +111,7 @@ var UIcontroller = (function(){
         inputButton: '.add__btn'
     }
 
+    //public functions
     return{
         getInput: function(){
             return{
@@ -69,6 +132,7 @@ var UIcontroller = (function(){
 //GLOBAL APP CONTROLLER
 var controller = (function(budgetCrtl, UICtrl){
 
+    //private functions
     var setupEventListeners = function(){
         var DOM = UICtrl.getDomStrings();
 
@@ -83,17 +147,22 @@ var controller = (function(budgetCrtl, UICtrl){
 
 
     var crtlAddItem = function(){
-        //TODO
-        //1. get the field input data
-        var input = UICtrl.getInput();
-        console.log(input);
 
+        var input, newItem;
+        
+        //1. get the field input data
+        input = UICtrl.getInput();
+        
         //2. add item to the budget controller
+        newItem = budgetCrtl.addItem(input.type, input.description, input.value);
         //3. add item to the user interface
+
+
         //4. calculate the budget
         //5. display budget in UI
     }
 
+    //public functions
     return{
         init: function(){
             console.log('Application started.');
